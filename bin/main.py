e14a26da359553d32db9858bb7db5ed977030aea
Fabio Henrique Gonzales Barbosa <fbarbosa@atech.com.br>
2017-06-16 00:46:26 -0300

    Adding log - Fixing error

--- bin/main.py
+++ bin/main.py
@@ -6,8 +6,8 @@ releases = DockerReleaseGenerator().generate()
 for r in releases:
   runner = DockerRunner(r.build_folder, r.generated_file, r.image_name, r.tag)
   runner.build()
-  logging.info('Image '%s:%s' builded', r.image_name, r.tag)
+  logging.info('Image "%s:%s" builded', r.image_name, r.tag)
 
 for r in releases:
   runner.push()
-  logging.info('Image '%s:%s' pushed', r.image_name, r.tag)
+  logging.info('Image "%s:%s" pushed', r.image_name, r.tag)
