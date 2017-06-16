bc7cb55b3e54703a4821cdaed682532d1c3acc20
Fabio Henrique Gonzales Barbosa <fbarbosa@atech.com.br>
2017-06-16 00:42:17 -0300

    Adding log

--- bin/main.py
+++ bin/main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-import os, sys
+import os, sys, logging
 from docker import DockerReleaseGenerator, DockerRunner
 
 releases = DockerReleaseGenerator().generate()
 for r in releases:
   runner = DockerRunner(r.build_folder, r.generated_file, r.image_name, r.tag)
   runner.build()
+  logging.info('Image '%s:%s' builded', r.image_name, r.tag)
 
 for r in releases:
   runner.push()
+  logging.info('Image '%s:%s' pushed', r.image_name, r.tag)
